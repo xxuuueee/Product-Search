c5f9e3b2464fd3c05393ee51b6ca4e23d1bda94f
Xue <xuemin2505@gmail.com>
2017-07-19 10:18:52 -0400

    Adding product searh app

---
+++ product-search/app/components/search.jsx
@@ -0,0 +1,42 @@
+var React = require("react");
+var SearchBar=require("./searchbar.jsx");
+var Results = require("./results.jsx");
+
+
+
+
+
+
+class Search extends React.Component {
+	constructor(props){
+		super(props);
+			this.state = {
+				query: " "
+			};
+		
+	}
+
+	handleQuery(query){
+		this.setState({"query": query.toLowerCase().trim()})
+		
+	}
+	render() {
+
+
+
+		return(
+
+			<div className="search">
+				<SearchBar onQuery={this.handleQuery.bind(this)}/>
+					
+				<Results products={this.props.products} query={this.state.query}/>
+					
+				</div>
+
+			
+
+		)
+	}
+}
+
+module.exports = Search;
---
+++ product-search/app/components/searchbar.jsx
@@ -0,0 +1,26 @@
+var React = require("react");
+
+
+
+
+class SearchBar extends React.Component{
+	
+	handleQuery(event){
+		this.props.onQuery(event.target.value);
+		}
+
+	render() {
+
+		return(
+			<div className="search-bar">
+				<input onChange={this.handleQuery.bind(this)} placeholder="Search"/>
+			</div>
+
+
+			)
+	}
+
+
+}
+
+module.exports = SearchBar;
